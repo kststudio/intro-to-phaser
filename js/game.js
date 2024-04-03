686dc438632f13db0e107df6ad3d832fc9e10b89
Kiki <kststudio@gmail.com>
2024-04-02 22:02:49 -0400

    lesson 03

--- js/game.js
+++ js/game.js
@@ -6,6 +6,7 @@ gameScene.preload = function () {
   // load images
   this.load.image("background", "./assets/background.png");
   this.load.image("player", "./assets/player.png");
+  this.load.image("enemy", "./assets/dragon.png");
 };
 
 // called once after the preload ends
@@ -14,18 +15,37 @@ gameScene.create = function () {
   let bg = this.add.sprite(0, 0, "background");
 
   // change the origin to the top-left corner
-  // bg.setOrigin(0, 0);
+  bg.setOrigin(0, 0);
+
+  // create the player
+  let player = this.add.sprite(70, 180, "player");
+
+  // we are reducing the width by 50%, and we are doubling the height
+  player.setScale(0.5, 2);
+
+  // create an enemy
+  let enemy1 = this.add.sprite(250, 180, "enemy");
+  enemy1.scaleX = 2;
+  enemy1.scaleY = 2;
+
+  // create a second enemy
+  let enemy2 = this.add.sprite(450, 180, "enemy");
+  enemy2.displayWidth = 300;
+
+  // flip
+  enemy1.flipX = true;
+  enemy1.flipY = true;
 
   // place sprite in the center
-  bg.setPosition(640 / 2, 360 / 2);
+  // bg.setPosition(640 / 2, 360 / 2);
 
-  let gameW = this.sys.game.config.width;
-  let gameH = this.sys.game.config.height;
+  // let gameW = this.sys.game.config.width;
+  // let gameH = this.sys.game.config.height;
 
-  console.log(gameW, gameH);
+  // console.log(gameW, gameH);
 
-  console.log(bg);
-  console.log(this);
+  // console.log(bg);
+  // console.log(this);
 };
 
 // set the configuration of the game
